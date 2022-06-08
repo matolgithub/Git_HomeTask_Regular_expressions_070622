8df489e82d3a6937bc2576e4c73bf9402b30b40c
Олег Матшин <89175955650@mail.ru>
2022-06-08 23:04:13 +0300

    Some changes of code in modify_phones_data().

--- HomeTask_RegEx_070622.py
+++ HomeTask_RegEx_070622.py
@@ -12,22 +12,26 @@ def read_csv_file():
 
 # TODO 1: выполните пункты 1-3 ДЗ
 # ваш код
-def create_names_data():
-    pass
-
 # приведение телефонов к форматам +7(999)999-99-99 и +7(999)999-99-99 доб.9999
 def modify_phones_data():
     contacts_list = read_csv_file()
-    correct_phone_list = []
+    newphone_contlist = []
     for i in contacts_list:
+        correct_phone_list = []
         for j in i:
             pattern = r"(\+7|8)?\s?\(?(\d+)\)?\s?\-?(\d{3})\-?(\d{2})\-?(\d{2}\,?)(\s*\(?(\доб.)?\s*(\d+)\)?)?"
             substitution = r"+7(\2)\3-\4-\5 \7\8"
             res = re.sub(pattern, substitution, j)
             correct_phone_list.append(res.strip())
-    pprint(correct_phone_list)
-    return correct_phone_list
+        newphone_contlist.append(correct_phone_list)
+    pprint(newphone_contlist)
+    return newphone_contlist
 
+# поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname соответственно
+def create_names_data():
+    pass
+
+# объединить все дублирующиеся записи о человеке в одну
 def delete_double_data():
     pass
 
@@ -45,3 +49,4 @@ if __name__ == '__main__':
     # read_csv_file()
     # write_new_csvfile()
     modify_phones_data()
+    # create_names_data()
